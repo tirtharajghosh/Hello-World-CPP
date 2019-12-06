80fbc9dd2333e31d6734e96249ce0a4a603de46a
tirtharajghosh <58604532+tirtharajghosh@users.noreply.github.com>
2019-12-06 23:00:58 +0530

    helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+#include<iostream>  
+  
+using namespace std; 
+  
+// main function - 
+// where the execution of program starts 
+int main() 
+{ 
+    // prints hello world 
+    cout<<"Hello World"; 
+      
+    return 0; 
+}
